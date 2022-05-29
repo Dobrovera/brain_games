802d7dcec961c6b4ff9f4fd66a4fce1ea5e5f77c
Vera Dobrodeeva <dobro.vera@gmail.com>
2022-05-29 20:49:16 +0300

    step 4

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+
 from brain_games import cli
 
-def main():
-	cli.welcome_user()
- 
-if __name__ == '__main__':
-	main()
 
+def main():
+    cli.welcome_user()
+
+
+if __name__ == '__main__':
+    main()
